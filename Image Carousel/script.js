3baff2d3e1195cd6e88e4975867f750a693e5c47
Laura Munteanu <laura.tarita@gmail.com>
2021-06-08 16:05:16 +0200

    Image Carousel with button ripple effect

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -38,3 +38,27 @@ leftBtn.addEventListener('click', () => {
   changeImage();
   resetInterval();
 })
+
+
+// add ripple effect to the buttons
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+  button.addEventListener('click', function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const buttonTop = event.target.offsetTop;
+    const buttonLeft = event.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement('span');
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  })
+})
