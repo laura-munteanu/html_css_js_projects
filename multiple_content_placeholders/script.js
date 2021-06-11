74a230fbf15ff2094000101368df7d1e4cd90a3e
Laura Munteanu <laura.tarita@gmail.com>
2021-06-11 23:53:00 +0200

    multiple content-placeholders

---
+++ multiple_content_placeholders/script.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+  if(window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+}
+
+// placeholders
+// const header = document.getElementById('header');
+// const title = document.getElementById('title');
+// const excerpt = document.getElementById('excerpt');
+// const profile_img = document.getElementById('profile_img');
+// const name = document.getElementById('name');
+// const date = document.getElementById('date');
+
+// const animated_bgs = document.querySelectorAll('.animated_bg');
+// const animated_bg_tests = document.querySelectorAll('.animated_bg_test');
+
+// setTimeout(getData, 1500);
+
+// function getData() {
+//   header.innerHTML = '<img src="/images/amden.jpg" alt="Amden">';
+//   title.innerHTML = 'Lorem ipsum dolor sit amet.';
+//   excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, id.';
+//   profile_img.innerHTML = ' <img src="/images/profile_picture.png" alt="profile picture">';
+//   name.innerHTML = 'John Doe';
+//   date.innerHTML = 'Jun 11 2021';
+
+//   animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+
+//   animated_bg.text.forEach(bg => bg.classList.remove('animated-bg-text'));
+// }
+
