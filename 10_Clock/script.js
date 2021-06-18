3281b6a34e9dbcb529fb5d1dc2b1eef31d81a6f7
Laura Munteanu <laura.tarita@gmail.com>
2021-06-18 11:25:05 +0200

    clock design changed

--- 10_Clock/script.js
+++ 10_Clock/script.js
@@ -5,6 +5,8 @@ const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
+const clock = document.querySelector('.clock');
+
 const days =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Oct","Nov", "Dec"];
 
@@ -13,10 +15,12 @@ toggle.addEventListener('click', (event) => {
   const html = document.querySelector('html');
   if(html.classList.contains('dark')) {
     html.classList.remove('dark');
-    event.target.innerHTML = 'Dark mode'
+    event.target.innerHTML = 'Dark mode';
+    clock.style.background = 'var(--background-light-gradient)';
   } else {
     html.classList.add('dark');
     event.target.innerHTML = 'Light mode';
+    clock.style.background = 'var(--background-dark-gradient)';
   }
 })
 
