c06f4a62e7765b93dce4390c53fc61474cf6798d
Laura Munteanu <laura.tarita@gmail.com>
2021-08-04 12:18:44 +0200

    auto text effect

---
+++ 21_auto_text_effect/script.js
@@ -0,0 +1,21 @@
+const textEL = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'We Love Programming!';
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+  textEL.innerText = text.slice(0, idx)
+
+  idx++;
+
+  if(idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (event) => speed = 300 / event.target.value);
